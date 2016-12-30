b2c446ead341948cc757a18f044f9c150c25dc34
Michael Jerger <michael.jerger@meissa-gmbh.de>
2016-12-30 09:19:38 +0100

    think about handling of only integration dependencies ...

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject org.domaindrivenarchitecture/dda-managed-vm "0.1.2-SNAPSHOT"
   :description "The managed vm desktop crate"
-  :url "https://meissa-gmbh.de"
-  :license {:name "meissa commercial license"
-            :url "https://www.meissa-gmbh.de"}
+  :url "https://www.domaindrivenarchitecture.org"
+  :license {:name "Apache License, Version 2.0"
+            :url "https://www.apache.org/licenses/LICENSE-2.0.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [com.palletops/pallet "0.8.12" :exclusions [org.clojure/tools.cli]]
                  [com.palletops/stevedore "0.8.0-beta.7"]
@@ -11,6 +11,7 @@
                  [org.domaindrivenarchitecture/dda-pallet "0.3.1-SNAPSHOT"]
                  [org.domaindrivenarchitecture/dda-pallet-commons "0.3.0"]
                  [org.domaindrivenarchitecture/dda-user-crate "0.3.5-SNAPSHOT"]
+                 [org.domaindrivenarchitecture/dda-init-crate "0.2.1-SNAPSHOT"]
                  [org.domaindrivenarchitecture/dda-backup-crate "0.4.1-SNAPSHOT"]]
   :repositories [["snapshots" :clojars]
                   ["releases" :clojars]]
@@ -23,8 +24,7 @@
              {:dependencies
               [[org.clojure/test.check "0.9.0"]
                [com.palletops/pallet "0.8.12" :classifier "tests"]
-               [org.domaindrivenarchitecture/dda-pallet-commons "0.3.0" :classifier "tests"]               
-               [org.domaindrivenarchitecture/dda-init-crate "0.2.1-SNAPSHOT"]
+               [org.domaindrivenarchitecture/dda-pallet-commons "0.3.0" :classifier "tests"]
                [org.domaindrivenarchitecture/pallet-aws "0.2.8-SNAPSHOT"]
                [ch.qos.logback/logback-classic "1.1.8"]
                [org.slf4j/jcl-over-slf4j "1.7.22"]]
