54cf411cb26c60a642f6fb51b798075e1396723b
Michael Jerger <michael.jerger@meissa-gmbh.de>
2017-03-04 18:03:43 +0100

    lein-release plugin: preparing 0.2.0 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.domaindrivenarchitecture/dda-managed-vm "0.2.0-SNAPSHOT"
+(defproject org.domaindrivenarchitecture/dda-managed-vm "0.2.0"
   :description "The managed vm desktop crate"
   :url "https://www.domaindrivenarchitecture.org"
   :license {:name "Apache License, Version 2.0"
@@ -8,7 +8,7 @@
                  [com.palletops/stevedore "0.8.0-beta.7"]
                  [prismatic/schema "1.1.3"]
                  [com.palletops/git-crate "0.8.0-alpha.2" :exclusions [org.clojure/clojure]]
-                 [org.domaindrivenarchitecture/dda-pallet "0.3.1-SNAPSHOT"]
+                 [org.domaindrivenarchitecture/dda-pallet "0.3.1"]
                  [org.domaindrivenarchitecture/dda-pallet-commons "0.3.0"]
                  [org.domaindrivenarchitecture/dda-user-crate "0.3.5-SNAPSHOT"]
                  [org.domaindrivenarchitecture/dda-init-crate "0.2.1-SNAPSHOT"]
@@ -28,13 +28,13 @@
                [com.palletops/pallet "0.8.12" :classifier "tests"]
                [org.domaindrivenarchitecture/dda-pallet-commons "0.3.0" :classifier "tests"]
                [org.domaindrivenarchitecture/pallet-aws "0.2.8-SNAPSHOT"]
-               [ch.qos.logback/logback-classic "1.1.8"]
-               [org.slf4j/jcl-over-slf4j "1.7.22"]]
+               [ch.qos.logback/logback-classic "1.2.1"]
+               [org.slf4j/jcl-over-slf4j "1.7.24"]]
               :plugins
               [[com.palletops/pallet-lein "0.8.0-alpha.1"]
                [lein-sub "0.3.0"]]}
               :leiningen/reply
-               {:dependencies [[org.slf4j/jcl-over-slf4j "1.7.22"]]
+               {:dependencies [[org.slf4j/jcl-over-slf4j "1.7.24"]]
                 :exclusions [commons-logging]}}
   :local-repo-classpath true
   :classifiers {:tests {:source-paths ^:replace ["test" "integration"]
