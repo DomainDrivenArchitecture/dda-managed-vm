8ea2a5efcfa8efc95779eea1438fd9c55f4a62df
Michael Jerger <michael.jerger@meissa-gmbh.de>
2017-10-21 18:33:52 +0200

    lein-release plugin: preparing 0.4.0 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject dda/dda-managed-vm "0.4.0-SNAPSHOT"
+(defproject dda/dda-managed-vm "0.4.0"
   :description "The managed vm desktop crate"
   :url "https://www.domaindrivenarchitecture.org"
   :license {:name "Apache License, Version 2.0"
