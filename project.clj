4765a9ef2e511c3d49c8665767fd637a63f0195f
jem <michael.jerger@meissa-gmbh.de>
2018-06-18 11:42:32 +0200

    use clojure 1.9

--- project.clj
+++ project.clj
@@ -1,14 +1,13 @@
-(defproject dda/dda-managed-vm "1.0.2-SNAPSHOT"
+(defproject dda/dda-managed-vm "1.1.0-SNAPSHOT"
   :description "The managed vm desktop crate"
   :url "https://www.domaindrivenarchitecture.org"
   :license {:name "Apache License, Version 2.0"
             :url "https://www.apache.org/licenses/LICENSE-2.0.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [dda/dda-pallet "2.1.1"]
-                 [dda/dda-user-crate "1.0.3-SNAPSHOT"]
-                 [dda/dda-serverspec-crate "1.0.4-SNAPSHOT"]
-                 [dda/dda-git-crate "1.1.0"]
-                 [dda/dda-backup-crate "0.7.1"]]
+  :dependencies [[dda/dda-pallet "2.2.0"]
+                 [dda/dda-user-crate "1.0.5"]
+                 [dda/dda-git-crate "1.1.3"]
+                 [dda/dda-backup-crate "1.0.4"]
+                 [dda/dda-serverspec-crate "1.1.0"]]
   :source-paths ["main/src"]
   :resource-paths ["main/resources"]
   :repositories [["snapshots" :clojars]
@@ -22,9 +21,10 @@
                                     "test/resources"]
                    :dependencies
                    [[org.clojure/test.check "0.10.0-alpha2"]
-                    [org.domaindrivenarchitecture/pallet-aws "0.2.8.2"]
-                    [com.palletops/pallet "0.8.12" :classifier "tests"]
-                    [dda/dda-pallet-commons "1.3.0" :classifier "tests"]
+                    [org.domaindrivenarchitecture/pallet-aws "0.2.8.2"
+                     :exclusions [com.palletops/pallet]]
+                    [dda/pallet "0.9.0" :classifier "tests"]
+                    [dda/dda-pallet-commons "1.3.1" :classifier "tests"]
                     [ch.qos.logback/logback-classic "1.3.0-alpha4"]
                     [org.slf4j/jcl-over-slf4j "1.8.0-beta1"]]
                    :plugins
@@ -32,15 +32,24 @@
                    :leiningen/reply
                    {:dependencies [[org.slf4j/jcl-over-slf4j "1.8.0-beta0"]]
                     :exclusions [commons-logging]}
-                   :repl-options {:init-ns dda.pallet.dda-managed-vm.app.instantiate-aws}}
+                   :repl-options {:init-ns dda.pallet.dda-managed-vm.app.instantiate-existing}}
              :test {:test-paths ["test/src"]
                     :resource-paths ["test/resources"]
-                    :dependencies [[com.palletops/pallet "0.8.12" :classifier "tests"]]}
+                    :dependencies [[dda/pallet "0.9.0" :classifier "tests"]]}
              :uberjar {:source-paths ["uberjar/src"]
                        :resource-paths ["uberjar/resources"]
                        :aot :all
                        :main dda.pallet.dda-managed-vm.main
-                       :dependencies [[org.clojure/tools.cli "0.3.5"]
+                       :dependencies [[org.clojure/tools.cli "0.3.7"]
                                       [ch.qos.logback/logback-classic "1.3.0-alpha4"]
                                       [org.slf4j/jcl-over-slf4j "1.8.0-beta1"]]}}
+  :release-tasks [["vcs" "assert-committed"]
+                  ["change" "version" "leiningen.release/bump-version" "release"]
+                  ["vcs" "commit"]
+                  ["vcs" "tag"]
+                  ["deploy"]
+                  ["uberjar"]
+                  ["change" "version" "leiningen.release/bump-version"]
+                  ["vcs" "commit"]
+                  ["vcs" "push"]]
   :local-repo-classpath true)
