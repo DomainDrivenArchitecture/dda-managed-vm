bd18c0afaefc39857b49590f3801aa5cb855c0d0
jem <michael.jerger@meissa-gmbh.de>
2018-07-17 20:23:39 +0200

    updated deps

--- project.clj
+++ project.clj
@@ -5,9 +5,9 @@
             :url "https://www.apache.org/licenses/LICENSE-2.0.html"}
   :dependencies [[dda/dda-pallet "2.2.1"]
                  [dda/dda-user-crate "1.0.5"]
-                 [dda/dda-git-crate "2.0.0"]
+                 [dda/dda-git-crate "2.0.1-SNAPSHOT"]
                  [dda/dda-backup-crate "1.0.5"]
-                 [dda/dda-serverspec-crate "1.1.3"]]
+                 [dda/dda-serverspec-crate "1.1.4-SNAPSHOT"]]
   :source-paths ["main/src"]
   :resource-paths ["main/resources"]
   :repositories [["snapshots" :clojars]
@@ -20,11 +20,11 @@
                    :resource-paths ["integration/resources"
                                     "test/resources"]
                    :dependencies
-                   [[org.clojure/test.check "0.10.0-alpha2"]
+                   [[org.clojure/test.check "0.10.0-alpha3"]
                     [dda/pallet "0.9.0" :classifier "tests"]
                     [dda/dda-pallet-commons "1.3.1" :classifier "tests"]
                     [ch.qos.logback/logback-classic "1.3.0-alpha4"]
-                    [org.slf4j/jcl-over-slf4j "1.8.0-beta1"]]
+                    [org.slf4j/jcl-over-slf4j "1.8.0-beta2"]]
                    :plugins
                    [[lein-sub "0.3.0"]]
                    :leiningen/reply
@@ -40,7 +40,7 @@
                        :main dda.pallet.dda-managed-vm.main
                        :dependencies [[org.clojure/tools.cli "0.3.7"]
                                       [ch.qos.logback/logback-classic "1.3.0-alpha4"]
-                                      [org.slf4j/jcl-over-slf4j "1.8.0-beta1"]]}}
+                                      [org.slf4j/jcl-over-slf4j "1.8.0-beta2"]]}}
   :release-tasks [["vcs" "assert-committed"]
                   ["change" "version" "leiningen.release/bump-version" "release"]
                   ["vcs" "commit"]
