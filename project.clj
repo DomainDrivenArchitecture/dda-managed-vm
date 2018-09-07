ccfa01b2bc2f43442bf620958e74f22c02b5f2db
jem <michael.jerger@meissa-gmbh.de>
2018-09-07 14:12:27 +0200

    use new serverspec

--- project.clj
+++ project.clj
@@ -4,10 +4,10 @@
   :license {:name "Apache License, Version 2.0"
             :url "https://www.apache.org/licenses/LICENSE-2.0.html"}
   :dependencies [[dda/dda-pallet "2.2.1"]
+                 [dda/dda-serverspec-crate "1.2.2-SNAPSHOT"]
                  [dda/dda-user-crate "1.0.5"]
                  [dda/dda-git-crate "2.1.0-SNAPSHOT"]
-                 [dda/dda-backup-crate "1.0.5"]
-                 [dda/dda-serverspec-crate "1.2.1"]]
+                 [dda/dda-backup-crate "1.0.5"]]
   :source-paths ["main/src"]
   :resource-paths ["main/resources"]
   :repositories [["snapshots" :clojars]
