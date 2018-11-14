8e2b663d5c073103bd536c3bacff143e0b2c0f5a
hel <lucca.hellriegel@meissa-gmbh.de>
2018-11-14 15:29:45 +0100

    lein-release plugin: bumped version from 2.2.2 to 2.2.3-SNAPSHOT for next development cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject dda/dda-managed-vm "2.2.2"
+(defproject dda/dda-managed-vm "2.2.3-SNAPSHOT"
   :description "The managed vm desktop crate"
   :url "https://www.domaindrivenarchitecture.org"
   :license {:name "Apache License, Version 2.0"
