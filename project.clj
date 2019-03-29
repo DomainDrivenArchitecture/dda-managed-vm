2a1f498870734c5d4b19b9f4dbf60d4b4cad18e7
jem <michael.jerger@meissa-gmbh.de>
2019-03-29 18:25:37 +0100

    updated dependencies

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
   :url "https://www.domaindrivenarchitecture.org"
   :license {:name "Apache License, Version 2.0"
             :url "https://www.apache.org/licenses/LICENSE-2.0.html"}
-  :dependencies [[dda/dda-pallet "2.2.2"]
+  :dependencies [[dda/pallet "0.9.1-SNAPSHOT"]
+                 [dda/dda-pallet "2.2.2"]
                  [dda/dda-serverspec-crate "1.3.0"]
                  [dda/dda-user-crate "1.0.6"]
                  [dda/dda-git-crate "2.2.0"]
@@ -20,11 +21,11 @@
                    :resource-paths ["integration/resources"
                                     "test/resources"]
                    :dependencies
-                   [[org.clojure/test.check "0.10.0-alpha3"]
-                    [dda/pallet "0.9.0" :classifier "tests"]
+                   [[org.clojure/test.check "0.10.0-alpha4"]
+                    [dda/pallet "0.9.1-SNAPSHOT" :classifier "tests"]
                     [dda/dda-pallet-commons "1.4.1" :classifier "tests"]
                     [ch.qos.logback/logback-classic "1.3.0-alpha4"]
-                    [org.slf4j/jcl-over-slf4j "1.8.0-beta2"]]
+                    [org.slf4j/jcl-over-slf4j "1.8.0-beta4"]]
                    :plugins
                    [[lein-sub "0.3.0"]]
                    :leiningen/reply
@@ -33,16 +34,16 @@
                    :repl-options {:init-ns dda.pallet.dda-managed-vm.app.instantiate-existing}}
              :test {:test-paths ["test/src"]
                     :resource-paths ["test/resources"]
-                    :dependencies [[dda/pallet "0.9.0" :classifier "tests"]]}
+                    :dependencies [[dda/pallet "0.9.1-SNAPSHOT" :classifier "tests"]]}
              :uberjar {:source-paths ["uberjar/src"]
                        :resource-paths ["uberjar/resources"]
                        :aot :all
                        :main dda.pallet.dda-managed-vm.main
                        :uberjar-name "dda-managed-vm-standalone.jar"
-                       :dependencies [[org.clojure/tools.cli "0.4.1"]
+                       :dependencies [[org.clojure/tools.cli "0.4.2"]
                                       [ch.qos.logback/logback-classic "1.3.0-alpha4"
                                        :exclusions [com.sun.mail/javax.mail]]
-                                      [org.slf4j/jcl-over-slf4j "1.8.0-beta2"]]}}
+                                      [org.slf4j/jcl-over-slf4j "1.8.0-beta4"]]}}
   :release-tasks [["vcs" "assert-committed"]
                   ["change" "version" "leiningen.release/bump-version" "release"]
                   ["vcs" "commit"]
