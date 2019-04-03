0d823ca8722e18b59fdd897229f544804bc89e59
jem <michael.jerger@meissa-gmbh.de>
2019-04-03 20:24:11 +0200

    prepare version for feature add

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject dda/dda-managed-vm "2.4.2-SNAPSHOT"
+(defproject dda/dda-managed-vm "2.5.0-SNAPSHOT"
   :description "The managed vm desktop crate"
   :url "https://www.domaindrivenarchitecture.org"
   :license {:name "Apache License, Version 2.0"
