b72c6ebf576a111e62fd5916c554bb802aea52ab
Clemens Geibel <clemens.geibel@meissa-gmbh.de>
2019-05-10 15:09:36 +0200

    Version 2.5.4-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject dda/dda-managed-vm "2.5.3"
+(defproject dda/dda-managed-vm "2.5.4-SNAPSHOT"
   :description "The managed vm desktop crate"
   :url "https://www.domaindrivenarchitecture.org"
   :license {:name "Apache License, Version 2.0"
