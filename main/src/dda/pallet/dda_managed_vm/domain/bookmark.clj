0a23113471701897f36edeb43895d6409216d98b
jem <michael.jerger@meissa-gmbh.de>
2019-05-23 17:05:37 +0200

    removed unused require

--- main/src/dda/pallet/dda_managed_vm/domain/bookmark.clj
+++ main/src/dda/pallet/dda_managed_vm/domain/bookmark.clj
@@ -13,9 +13,7 @@
 ; WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 ; See the License for the specific language governing permissions and
 ; limitations under the License.
-(ns dda.pallet.dda-managed-vm.domain.bookmark
-  (:require
-    [schema.core :as s]))
+(ns dda.pallet.dda-managed-vm.domain.bookmark)
 
 (def default-bookmarks
   [{:name "Bookmarks Toolbar"
