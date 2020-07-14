6d84e5ce8f3f326d1b499954a495deaf3f6b30db
jem <michael.jerger@meissa-gmbh.de>
2020-07-14 18:36:13 +0200

    updated deps & use pyb for integration test

---
+++ integration/docker/build.py
@@ -0,0 +1,42 @@
+from os import environ
+from pybuilder.core import task, init
+from ddadevops import *
+import logging
+
+name = 'dda-managed-vm'
+MODULE = 'docker'
+PROJECT_ROOT_PATH = '../..'
+
+class MyBuild(DevopsDockerBuild):
+    pass
+
+import sys
+from subprocess import run
+@init
+def initialize(project):
+    print (sys.version_info)
+    project.build_depends_on('ddadevops>=0.6.1')
+    stage = 'notused'
+    dockerhub_user = 'notused'
+    dockerhub_password = 'notused'
+    config = create_devops_docker_build_config(
+        stage, PROJECT_ROOT_PATH, MODULE, dockerhub_user, dockerhub_password)
+    build = MyBuild(project, config)
+    build.initialize_build_dir()
+    run('cp ' + PROJECT_ROOT_PATH + '/target/uberjar/dda-managed-vm-standalone.jar ' + 
+        build.build_path() + '/image/', shell=True)
+
+@task
+def image(project):
+    build = get_devops_build(project)
+    build.image()
+
+@task
+def drun(project):
+    build = get_devops_build(project)
+    build.drun()
+
+@task
+def test(project):
+    build = get_devops_build(project)
+    build.test()
--- project.clj
+++ project.clj
@@ -3,10 +3,10 @@
   :url "https://www.domaindrivenarchitecture.org"
   :license {:name "Apache License, Version 2.0"
             :url "https://www.apache.org/licenses/LICENSE-2.0.html"}
-  :dependencies [[dda/dda-pallet "3.1.1"]
+  :dependencies [[dda/dda-pallet "3.1.2"]
                  [dda/dda-serverspec-crate "1.3.4"]
-                 [dda/dda-user-crate "2.1.1"]
-                 [dda/dda-git-crate "2.2.4"]
+                 [dda/dda-user-crate "2.1.2"]
+                 [dda/dda-git-crate "2.2.5"]
                  [dda/dda-backup-crate "1.0.7"]]
   :source-paths ["main/src"]
   :resource-paths ["main/resources"]
@@ -21,10 +21,10 @@
                    :resource-paths ["integration/resources"
                                     "test/resources"]
                    :dependencies
-                   [[org.clojure/test.check "1.0.0"]
+                   [[org.clojure/test.check "1.1.0"]
                     [dda/pallet "0.9.1" :classifier "tests"]
                     [dda/data-test "0.1.1"]
-                    [dda/dda-pallet-commons "1.6.3" :classifier "tests"]
+                    [dda/dda-pallet-commons "1.6.4" :classifier "tests"]
                     [ch.qos.logback/logback-classic "1.3.0-alpha5"]
                     [org.slf4j/jcl-over-slf4j "2.0.0-alpha1"]]
                    :plugins
